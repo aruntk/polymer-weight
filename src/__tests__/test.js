62e5a9b5124d1741361e2a790f7893bc4bd491c7
aruntk <aruntknambiar@gmail.com>
2017-01-28 20:44:46 +0530

    init

---
+++ src/__tests__/test.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import weigh from '..';
+
+describe('module name', function () {
+  it('returns module name', function () {
+    assert.equal('polymer-weight', weigh);
+  });
+});
