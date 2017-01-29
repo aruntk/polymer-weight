d308511deac929aacf19ed71a60256ba8624fb16
aruntk <aruntknambiar@gmail.com>
2017-01-30 04:38:36 +0530

    depth options added. standalone weight added.

--- README.md
+++ README.md
@@ -10,11 +10,45 @@ npm i -g polymer-weight
 weigh --path path/to/file.html
 ```
 
+## Options
+
+### Path
+
+path to the starting file (index file)
+```sh
+weigh --path path/to/file.html
+```
+### Depth
+
+How deep the table should show.
+
+eg if depth = 1. Immediate child links and the index file
+```sh
+weigh --path path/to/file.html --depth 1
+```
+
+## Table Columns
+
+### Standalone weight
+
+Standalone size of the components. ie. space that can be saved by removing the component.
+
+### Net weight
+
+Net weight of a link.
+
+### file size
+
+Size of the file corresponding to a link.
+
 More features coming. :)
+
 
 ## TODO
 
 - [x] Deep Scanning of files (walk).
-- [ ] Net weight added by component and its children.
+- [x] Net weight added by component and its children.
+- [x] Standalone size (space that can be saved by removing the component)
 - [ ] Show dependency relationship in some way so that user can know which link to remove to reduce size.
 - [ ] Handle css imports.
+
--- src/cli.js
+++ src/cli.js
@@ -13,6 +13,7 @@ const cli = meow(`
 
     Options
       --path, -P  Path to starting file/folder
+      --depth, -d  How deep should the table show
 
     Examples
       $ weigh --path src/index.html
@@ -21,10 +22,11 @@ const cli = meow(`
 const flags = cli.flags;
 
 let sourcePath = flags.path ? path.resolve(flags.path): process.cwd();
+const tDepth = flags.depth;
 const store = {};
 const table = new Table({
-  head: ['Type', 'Link', 'Size'],
-  colWidths: [25, 100, 25],
+  head: ['Type', 'Link', 'Net Weight', 'Standalone Weight', 'file size'],
+  colWidths: [10, 80, 15, 20, 15],
   style: { head: ['red'], border: ['white'] },
   chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' }
 });
@@ -33,35 +35,108 @@ try {
   const stat = fs.statSync(sourcePath);
   const isDir = stat.isDirectory();
   const indexPath = isDir ? path.resolve(sourcePath, './index.html') : sourcePath;
-  const store = {};
   parseFile(indexPath, store).then((r) => {
+    const localLinks = _.filter(_.values(store), ast => ast.urlObject.local);
+    const indexWeight = calcOtherWeights(localLinks, indexPath, stat.size);
+    const fSize = filesize(stat.size, { base: 10 });
+    const nSize = filesize(indexWeight.nSize, { base: 10 });
     const astColl = _.values(store);
-    const group = _.groupBy(astColl, ast => ast.urlObject.local ? 'local': 'external');
-    if(!group.local && !group.external) {
-      console.log('No Links Found');
-      return;
+    let visibleColl;
+
+    if(tDepth) {
+      let visibleLinks = [indexPath];
+      for(let i = 0; i < tDepth; i += 1) {
+        visibleLinks = visibleLinks.concat(walkLinks(astColl, visibleLinks));
+      }
+      visibleColl = _.values(_.pick.apply(null, [store].concat(visibleLinks)));
     }
-    const sortedColl = _.sortBy(group.local, [function(ast) {
-      return ast.fileStats.size;
+
+    const group = _.groupBy(visibleColl || astColl, ast => ast.urlObject.local ? 'local': 'external');
+
+    const tableArray = toTableArray(group, stat.size, indexPath);
+
+    const sortedTable = _.sortBy(tableArray, [function(a) {
+      return a.detail.size;
     }]);
-    let netIndexSize = stat.size;
-    _.each(sortedColl.reverse(), (ast) => {
-      netIndexSize += ast.fileStats.size;
-      const size = filesize(ast.fileStats.size, { base: 10 });
-      const absPath = ast.urlObject.absPath;
-      const relPath = path.relative(path.dirname(indexPath), absPath);
-      table.push([ast.tagName, relPath, size]);
+
+    sortedTable.push({
+      arr : ['Total', path.basename(indexPath), nSize, 'NA', fSize],
+      detail: { size: nSize },
     });
-    _.each(group.external, (ast) => {
-      const absPath = ast.urlObject.absPath;
-      table.push([ast.tagName, absPath, 'NA']);
+
+    _.each(sortedTable.reverse(), function(a) {
+      table.push(a.arr);
     });
-    table.push(['Total', path.basename(indexPath), filesize(netIndexSize, { base: 10 })]);
+
     console.log(table.toString());
   });
 }
 catch(e) {
   console.log(e);
+}
+
+function walkLinks(links, filePaths) {
+
+  const immChildren = _.filter(links, (link) => {
+    return _.find(link.occurances, (o) => {
+      return filePaths.indexOf(o.filePath) !== -1;
+    });
+  });
+
+  return _.map(immChildren, (link) => {
+    return link.urlObject.absPath;
+  });
+
+}
+
+function calcOtherWeights(links, filePath, fSize) {
+  let nSize = 0;
+  let sSize = 0;
+  const immChildren = _.filter(links, (link) => {
+    return _.find(link.occurances, (o) => {
+      return o.filePath === filePath;
+    });
+  });
+  if(immChildren) {
+    _.each(immChildren, (c) => {
+      const childSize = calcOtherWeights(links, c.urlObject.absPath, c.fileStats.size);
+      nSize += childSize.sSize;
+      if(c.occurances.length === 1) {
+        sSize += childSize.sSize;
+      }
+    });
+  }
+  nSize += fSize;
+  sSize += fSize;
+  if(store[filePath]) {
+    store[filePath].fileStats.nSize = nSize;
+    store[filePath].fileStats.sSize = sSize;
+  }
+  return { nSize, sSize };
+}
+function toTableArray(group, initialIndexSize, indexPath) {
+  let netIndexSize = initialIndexSize;
+  const arr = [];
+  _.each(group.local, (ast) => {
+    netIndexSize += ast.fileStats.size;
+    const fSize = filesize(ast.fileStats.size, { base: 10 });
+    const nSize = filesize(ast.fileStats.nSize, { base: 10 });
+    const sSize = filesize(ast.fileStats.sSize, { base: 10 });
+    const absPath = ast.urlObject.absPath;
+    const relPath = path.relative(path.dirname(indexPath), absPath);
+    arr.push({
+      arr : [ast.tagName, relPath, nSize, sSize, fSize],
+      detail: { size: ast.fileStats.sSize  },
+    });
+  });
+  _.each(group.external, (ast) => {
+    const absPath = ast.urlObject.absPath;
+    arr.push({
+      arr : [ast.tagName, absPath, 'NA', 'NA', 'NA'],
+      detail: { size: 0 },
+    });
+  });
+  return arr;
 }
 
 
